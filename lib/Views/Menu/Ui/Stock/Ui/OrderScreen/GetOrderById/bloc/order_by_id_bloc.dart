82b9a582fd539e1698f2efb959c3eabcc46679f0
HP <basirkhan.hashemi@gmail.com>
2026-04-23 23:31:45 +0430

    home commit

--- lib/Views/Menu/Ui/Stock/Ui/OrderScreen/GetOrderById/bloc/order_by_id_bloc.dart
+++ lib/Views/Menu/Ui/Stock/Ui/OrderScreen/GetOrderById/bloc/order_by_id_bloc.dart
@@ -73,8 +73,6 @@ class OrderByIdBloc extends Bloc<OrderByIdEvent, OrderByIdState> {
     ));
   }
 
-
-
   Future<void> _onLoadOrderById(LoadOrderByIdEvent event, Emitter<OrderByIdState> emit,) async {
     emit(OrderByIdLoading());
 
